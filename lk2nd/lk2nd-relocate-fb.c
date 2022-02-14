775c4a781c137def2e0711214aa63218ab1532c6
Jack Matthews <jm5112356@gmail.com>
2022-02-14 13:38:54 -0500

    Initial lk2nd-relocate-fb
    Could be improved, but it works

---
+++ lk2nd/lk2nd-relocate-fb.c
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: GPL-2.0-only
+
+#include <libfdt.h>
+#include <bits.h>
+#include <reg.h>
+#include <mdp5.h>
+#include <platform/iomap.h>
+
+void lk2nd_relocate_fb(const void *fdt, int offset)
+{
+	int len; 
+	const uint32_t *val;
+	uint32_t new;
+
+	val = fdt_getprop(fdt, offset, "lk2nd,relocate-fb", &len);
+
+	if (len < 0) {
+		return;
+	}
+	
+	new = fdt32_to_cpu(*val);
+	
+	// Assume the pipe for now
+	writel(new, MDP_VP_0_RGB_0_BASE + PIPE_SSPP_SRC0_ADDR);
+	writel(BIT(3), MDP_CTL_0_BASE + CTL_FLUSH);
+	dprintf(INFO, "Relocated framebuffer to %p, begone rainbow screen!\n", new);
+}
