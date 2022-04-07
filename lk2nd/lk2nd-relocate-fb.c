66792aa0bc4c31aa6e4c066f78fceb937b410d73
Dominik Kobinski <dominikkobinski314@gmail.com>
2022-04-07 17:46:27 +0200

    lk2nd: relocate-fb: add mdp3 relocation

--- lk2nd/lk2nd-relocate-fb.c
+++ lk2nd/lk2nd-relocate-fb.c
@@ -3,7 +3,11 @@
 #include <libfdt.h>
 #include <bits.h>
 #include <reg.h>
+#if DISPLAY_TYPE_MDP3
+#include <mdp3.h>
+#else
 #include <mdp5.h>
+#endif
 #include <platform/iomap.h>
 
 void lk2nd_relocate_fb(const void *fdt, int offset)
@@ -17,11 +21,16 @@ void lk2nd_relocate_fb(const void *fdt, int offset)
 	if (len < 0) {
 		return;
 	}
-	
+
 	new = fdt32_to_cpu(*val);
 	
+	#if DISPLAY_TYPE_MDP3
+	writel(new, MDP_DMA_P_BUF_ADDR);
+	dprintf(INFO, "Relocated framebuffer to %p, begone rainbow screen!\n", new);
+	#else
 	// Assume the pipe for now
 	writel(new, MDP_VP_0_RGB_0_BASE + PIPE_SSPP_SRC0_ADDR);
 	writel(BIT(3), MDP_CTL_0_BASE + CTL_FLUSH);
 	dprintf(INFO, "Relocated framebuffer to %p, begone rainbow screen!\n", new);
+	#endif
 }
